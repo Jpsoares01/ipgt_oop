a3f7d9999c86f672b894265e9fa10cc0f7b04dca
Jpsoares01 <jpssoares23@icloud.com>
2025-12-30 02:37:52 +0000

    project setup

---
+++ Core/ObsevableObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace mp_anapp.Core
+{
+    class ObsevableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Core/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace mp_anapp.Core
+{
+    class RelayCommand : ICommand
+    {
+        private Action<object> _executeAction;
+        private Func<object, bool> _canExecuteFunc;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> executeAction, Func<object, bool> canExecuteFunc)
+        {
+            _executeAction = executeAction;
+            _canExecuteFunc = canExecuteFunc;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecuteFunc == null || _canExecuteFunc(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeAction(parameter);
+        }
+    }
+}
