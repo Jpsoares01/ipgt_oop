88b54842206f5032a4997f44ee92492817477fba
Jpsoares01 <jpssoares23@icloud.com>
2026-01-08 17:55:10 +0000

    unfinished login/registry and home views

--- MVVM/ViewModels/MainViewModel.cs
+++ MVVM/ViewModels/MainViewModel.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ipgt_oop.MVVM.ViewModels
 {
     internal class MainViewModel : ObservableObject
     {
         public LoginViewModel LoginVM { get; set; }
+        public RegistryViewModel RegistryVM { get; set; }
 
         private object _currentView;
 
@@ -23,10 +25,19 @@ namespace ipgt_oop.MVVM.ViewModels
             }
         }
 
+        public ICommand ShowLoginCommand { get; }
+        public ICommand ShowRegistryCommand { get; }
+
         public MainViewModel()
         {
             LoginVM = new LoginViewModel();
+            RegistryVM = new RegistryViewModel();
+
             CurrentView = LoginVM;
+
+            ShowLoginCommand = new RelayCommand(o => CurrentView = LoginVM, o => true);
+            ShowRegistryCommand = new RelayCommand(o => CurrentView = RegistryVM, o => true);
+
         }
     }
 }
---
+++ MVVM/ViewModels/RegistryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using mp_anapp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ipgt_oop.MVVM.ViewModels
+{
+    internal class RegistryViewModel : ObservableObject
+    {
+    }
+}
