8cd41065f3c95b74997fef5a1206fb27b1206f00
João Borges <joaoapborges@gmai.com>
2026-01-17 23:28:50 +0000

    primeira conexao

--- MVVM/ViewModels/RegistryViewModel.cs
+++ MVVM/ViewModels/RegistryViewModel.cs
@@ -6,9 +6,38 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
+using System.Collections.ObjectModel; 
+using ipgt_oop.MVVM.Models;           
+using ipgt_oop.Services;              
+
 namespace ipgt_oop.MVVM.ViewModels
 {
     internal class RegistryViewModel : ObservableObject
     {
+
+        public ObservableCollection<Bank> BankList { get; set; }
+
+        public RegistryViewModel()
+        {
+            BankList = new ObservableCollection<Bank>();
+
+            LoadBanks();
+        }
+
+        private async void LoadBanks()
+        {
+            var api = new ApiService();
+
+            var banks = await api.GetBanksAsync();
+            foreach (var bank in banks)
+            {
+                BankList.Add(bank);
+            }
+
+        }
+
+
+
+
     }
 }
---
+++ Services/ApiService.cs
@@ -0,0 +1,54 @@
+﻿using ipgt_oop.MVVM.Models;
+using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace ipgt_oop.Services
+{
+    class ApiService
+    {
+        private static readonly HttpClient _client = new HttpClient();
+        private const string BaseUrl = "http://localhost:8080/";
+
+        public ApiService()
+        {
+
+            if (_client.BaseAddress == null)
+            {
+                _client.BaseAddress = new Uri(BaseUrl);
+            }
+        }
+
+        // metodos api
+
+        public async Task<List<Bank>> GetBanksAsync()
+        {
+            try 
+            {
+                var lista = await _client.GetFromJsonAsync<List<Bank>>("api/Bank");
+
+                return lista ?? new List<Bank>();
+            }
+
+            catch (Exception ex)
+            {
+                return new List<Bank>();
+            }
+
+            }
+
+        
+
+
+        
+
+
+
+
+    }
+}
